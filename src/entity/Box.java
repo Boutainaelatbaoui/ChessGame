1a15626f007eecc73b748e5b905ea9313f04d718
Boutainaelatbaoui <boutaina.elatbaoui1998@gmail.com>
2023-09-21 20:19:28 +0100

    Added Box class

---
+++ src/entity/Box.java
@@ -0,0 +1,38 @@
+package entity;
+
+public class Box {
+    private int x;
+    private int y;
+    private Piece piece;
+
+    public Box(int x, int y, Piece piece) {
+        this.setPiece(piece);
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+}
+
