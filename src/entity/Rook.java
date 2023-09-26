a9134306f5194dc23f47de9e3cecad28cb7580ab
Boutainaelatbaoui <boutaina.elatbaoui1998@gmail.com>
2023-09-26 22:19:47 +0100

    Implemented 'moveValid' logic for the Rook class

--- src/entity/Rook.java
+++ src/entity/Rook.java
@@ -9,34 +9,15 @@ public class Rook extends Piece {
         return "R";
     }
     public boolean moveValid(int startX, int startY, int endX, int endY, Piece destPiece) {
-        int deltaX = Math.abs(endX - startX);
-        int deltaY = endY - startY;
+        int xChange = Math.abs(endX - startX);
+        int yChange = Math.abs(endY - startY);
 
-        // White pawn moves forward (up the board)
-        if (isWhite()) {
-            if (deltaY == 1 && deltaX == 0) {
-                // Pawn moves one square forward
-                return true;
-            } else if (deltaY == 2 && deltaX == 0 && startY == 1) {
-                // Pawn's first move, moving two squares forward
-                return true;
-            } else if (deltaY == 1 && deltaX == 1) {
-                // Pawn captures diagonally
-                return true;
-            }
+        if (destPiece != null && destPiece.isWhite() == this.isWhite()) {
+            return false;
         }
-        // Black pawn moves forward (down the board)
-        else {
-            if (deltaY == -1 && deltaX == 0) {
-                // Pawn moves one square forward
-                return true;
-            } else if (deltaY == -2 && deltaX == 0 && startY == 6) {
-                // Pawn's first move, moving two squares forward
-                return true;
-            } else if (deltaY == -1 && deltaX == 1) {
-                // Pawn captures diagonally
-                return true;
-            }
+
+        if (xChange == 0 || yChange == 0) {
+            return true;
         }
 
         return false;
